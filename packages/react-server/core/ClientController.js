55073ba3bfff7a02b135a2187ec8b779392b469b
Sresan Thevarajah <sresan.thevarajah@redfin.com>
2016-10-06 16:25:41 -0700

    Remove default new Date param

--- packages/react-server/core/ClientController.js
+++ packages/react-server/core/ClientController.js
@@ -82,7 +82,7 @@ class ClientController extends EventEmitter {
 		this._failDfd = Q.defer();
 
 		// Log this after loglevel is set.
-		logTimingData('wakeFromStart', window._reactServerTimingStart, new Date);
+		logTimingData('wakeFromStart', window._reactServerTimingStart);
 		// this is a proxy for when above the fold content gets painted (displayed) on the browser
 		logTimingData('displayAboveTheFold.fromStart', window._reactServerTimingStart, window.__displayAboveTheFold);
 	}
